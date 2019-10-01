b4176d456b2eea31ca7296bc6b9224334478a257
pc <pc@DESKTOP-ETSQ5UP>
2019-10-01 15:03:24 +0300

    Calculator completed

--- SimpleCalculator/App.js
+++ SimpleCalculator/App.js
@@ -6,41 +6,130 @@
  * @flow
  */
 
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import {
   StyleSheet,
   View,
   Text,
+  TextInput,
   StatusBar,
+  Button,
 } from 'react-native';
+import Header from './src/components/header';
 
 export default class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      input1: '',
+      input2: '',
+      result:''
+    };
+    this.sum=this.sum.bind(this);
+    this.deduct=this.deduct.bind(this);
+    this.mult=this.mult.bind(this);
+    this.div=this.div.bind(this);
+  }
+  sum(){
+    const number1=parseFloat(this.state.input1);
+    const number2=parseFloat(this.state.input2);
+    const result=number1+number2;
+    this.setState({
+      result:result
+    })
+  }
+
+  deduct(){
+    const number1=parseFloat(this.state.input1);
+    const number2=parseFloat(this.state.input2);
+    const result=number1-number2;
+    this.setState({
+      result:result
+    })
+  }
+
+  mult(){
+    const number1=parseFloat(this.state.input1);
+    const number2=parseFloat(this.state.input2);
+    const result=number1*number2;
+    this.setState({
+      result:result
+    })
+  }
+
+  div(){
+    const number1=parseFloat(this.state.input1);
+    const number2=parseFloat(this.state.input2);
+    const result=number1/number2;
+    this.setState({
+      result:result
+    })
+  }
   render() {
     return (
-    <>
-      <StatusBar barStyle="dark-content" />
-      <View style={styles.container}>
-        <Text style={styles.welcome}>Simple Calculator</Text>
-      </View> 
-    </>   
+      <>
+        <StatusBar barStyle="dark-content" />
+        <View style={styles.container}>
+          <Header headerText='Simple Calculator'/>
+          <View style={styles.contentWrapper}>
+            <TextInput
+              style={styles.input}
+              placeholder="First Number"
+              onChangeText={text => {
+                this.setState({
+                  input1: text,
+                });
+              }}
+              value={this.state.input1}></TextInput>
+
+            <View style={styles.buttonWrapper}>
+              <Button onPress={this.sum} color="#841584" title="+"></Button>
+              <Button onPress={this.deduct} color="#841584" title="-"></Button>
+              <Button onPress={this.mult} color="#841584" title="*"></Button>
+              <Button onPress={this.div} color="#841584" title="/"></Button>
+            </View> 
+
+            <TextInput
+              style={styles.input}
+              placeholder="Second Number"
+              onChangeText={text => {
+                this.setState({
+                  input2: text,
+                });
+              }}
+              value={this.state.input2}></TextInput>
+            <Text style={styles.simpleText}>
+              Result: {this.state.result}
+            </Text>
+          </View>
+        </View>
+      </>
     );
   }
 }
 
 const styles = StyleSheet.create({
   container: {
-    height: 80,
-    shadowColor: '#000000',
-    shadowOffset: { width: 0, height: 4 },
-    shadowOpacity: 2,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#f5fcff'
+    flex: 1,
+    flexDirection: 'column',
   },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-  }
+  contentWrapper: {
+    padding: 20,
+    flexDirection: 'column',
+    justifyContent: 'center',
+  },
+  buttonWrapper:{
+    flexDirection:'row',
+    justifyContent:'space-evenly',
+    
+  },
+  input: {
+    height: 40,
+  },
+  simpleText: {
+    height: 30,
+    fontSize: 14,
+  },
+  
 });
-
-
---
+++ SimpleCalculator/src/components/header.js
@@ -0,0 +1,28 @@
+import React, {Component} from 'react';
+import {StyleSheet, Text,View} from 'react-native';
+export default class header extends Component {
+  render() {
+      const{header,headerText}=styles;
+    return (
+      <View style={header}>
+        <Text style={headerText}>{this.props.headerText}</Text>
+      </View>
+    );
+  }
+  
+}
+const styles = StyleSheet.create({
+header: {
+    height: 80,
+    shadowColor: '#000000',
+    shadowOffset: {width: 0, height: 4},
+    shadowOpacity: 2,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#f5fcff',
+  },
+  headerText: {
+    fontSize: 20,
+    textAlign: 'center',
+  },
+});
