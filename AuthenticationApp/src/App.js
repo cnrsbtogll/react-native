32a78cd295ba8e779b6c092e6bd431fe654ae8d6
pc <pc@DESKTOP-ETSQ5UP>
2019-10-04 14:16:20 +0300

    show error message

--- AuthenticationApp/src/App.js
+++ AuthenticationApp/src/App.js
@@ -1,15 +1,30 @@
 import React, { Component } from 'react';
-import {Text, View, StyleSheet} from 'react-native';
+import {KeyboardAvoidingView, View, StyleSheet} from 'react-native';
+import firebase from 'firebase';
 import Banner from './components/banner';
 import LoginForm from './components/loginForm';
 
 export default class App extends Component {
+    componentDidMount(){
+        firebase.initializeApp(
+            {
+                apiKey: "AIzaSyBvFUuxK5ZBL-nLEP7yRcYlJUgLoK8Id60",
+                authDomain: "authentication-4de89.firebaseapp.com",
+                databaseURL: "https://authentication-4de89.firebaseio.com",
+                projectId: "authentication-4de89",
+                storageBucket: "",
+                messagingSenderId: "17694656237",
+                appId: "1:17694656237:web:ed2936517cc6022649aee4",
+                measurementId: "G-3VTWBKG0RG"
+              }
+        )
+    }
     render() {
         return (
-            <View style={styles.appContainer}>
+            <KeyboardAvoidingView style={styles.appContainer}>
                 <Banner text='Authentication'></Banner>
                 <LoginForm></LoginForm>
-            </View>
+            </KeyboardAvoidingView>
         )
     }
 }
--- AuthenticationApp/src/components/loginForm.js
+++ AuthenticationApp/src/components/loginForm.js
@@ -1,14 +1,39 @@
 import React, { Component } from 'react';
-import {View,Button,Text} from 'react-native';
+import {KeyboardAvoidingView,View,Button,Text,StyleSheet} from 'react-native';
+import firebase from 'firebase';
 import {Input} from './common';
 class LoginForm extends Component {
     state={
         email:'',
         password:'',
+        error:'',
+    }
+    onButtonClicked(){
+        const {email,password}=this.state;
+        this.setState({
+            error:''
+        })
+        firebase.auth().signInWithEmailAndPassword(email,password).catch((err)=>{
+           
+            firebase.auth().createUserWithEmailAndPassword(email,password).catch((error)=>{
+                
+                console.log('hata'); //TODO 
+                this.setState({
+                    error:'Authentication Failed'
+                })
+            })
+        });
     }
     render() {
+        const {error}=this.state;
+        const errMsg=error?(
+            <Text style={styles.errorStyle}>
+                {error}
+            </Text>
+        ): null;
+        
         return (
-            <View style={{padding:30}}>
+            <KeyboardAvoidingView style={{padding:30}}>
                 <View>
                     <Input text='Email' inputPlaceHolder='Enter Email'
                                 onChangeText={(text)=>{
@@ -26,15 +51,24 @@ class LoginForm extends Component {
                                     password:text
                                 })            
                                 }}
-                                secureTextEntryc
+                                secureTextEntry
                                 value={this.state.password}/>
                     
                 </View>
+                {errMsg}
                 <View>
-                    <Button color='#E87B79' title='Login'></Button>
+                    <Button onPress={this.onButtonClicked.bind(this)} color='#E87B79' title='Login'></Button>
                 </View>
-            </View>
+            </KeyboardAvoidingView>
         )
     }
 }
+const styles=StyleSheet.create({
+    errorStyle:{
+        fontSize:20,
+        color:'red',
+        paddingTop:5,
+        alignSelf:'center',
+    }
+})
 export default LoginForm;
